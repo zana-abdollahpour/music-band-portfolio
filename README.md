6a5429cb814c9da93db6e2a9c62d439dd58d8c89
zana-abdollahpour <zana.ap4@gmail.com>
2025-02-09 18:11:34 +0330

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# LaMusica (React + TypeScript + Vite + Tailwind v4)
+
+This is a fictional music band portfolio website
---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [tailwindcss(), react()],
+  resolve: {
+    alias: { "@": path.resolve(__dirname, "./src") },
+  },
+});
