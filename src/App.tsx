b6895aa71e1dbeba2b7e48f3653ce775a43705e2
zana-abdollahpour <zana.ap4@gmail.com>
2025-02-10 22:50:34 +0330

    add countdown section

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/hero";
 import Albums from "@/components/albums";
 import Promotion from "@/components/promotion";
 import Discography from "@/components/discography";
+import CountDown from "@/components/count-down";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Albums />
         <Promotion />
         <Discography />
+        <CountDown />
       </main>
     </>
   );
