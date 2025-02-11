f50549265bb2d9a499c39340cf27f38295625fea
zana-abdollahpour <zana.ap4@gmail.com>
2025-02-11 13:09:51 +0330

    refactor: remove extra App component

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,32 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
-import App from "@/App";
+import Header from "@/components/hero";
+import Albums from "@/components/albums";
+import Promotion from "@/components/promotion";
+import Discography from "@/components/discography";
+import CountDown from "@/components/count-down";
+import BandMembers from "@/components/band-members";
+import UpcomingTours from "@/components/upcoming-tours";
+import Newsletter from "@/components/newsletter";
+import Presale from "@/components/presale";
+import Gallery from "@/components/gallery";
+import Footer from "@/components/footer";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Header />
+    <main>
+      <Albums />
+      <Promotion />
+      <Discography />
+      <CountDown />
+      <BandMembers />
+      <UpcomingTours />
+      <Newsletter />
+      <Presale />
+      <Gallery />
+    </main>
+    <Footer />
   </StrictMode>,
 );
